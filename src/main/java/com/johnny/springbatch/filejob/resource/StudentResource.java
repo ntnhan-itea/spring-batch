2c57065e4837a713548943466d782b78b5930bf2
Nhan Nguyen <nhannguyen039@gmail.com>
2024-06-11 23:37:46 +0700

    added swagger-ui

--- src/main/java/com/johnny/springbatch/filejob/resource/StudentResource.java
+++ src/main/java/com/johnny/springbatch/filejob/resource/StudentResource.java
@@ -1,5 +1,9 @@
 package com.johnny.springbatch.filejob.resource;
 
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -9,6 +13,7 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +26,16 @@ public class StudentResource {
     private final JobLauncher jobLauncher;
     private final Job runJob22;
 
+    @GetMapping
+    @Operation(summary = "ping")
+    public String ping() {
+        return "pong";
+    }
+
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The job run succeeded"),
+    })
     public void importCsvToDBJob() {
         JobParameters jobParameters = new JobParametersBuilder()
                 .addLong("startAt", System.currentTimeMillis())
